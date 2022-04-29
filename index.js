a780656dff17c2bdc1119241512825064c086077
Jamaal <jjrog16@gmail.com>
2022-04-29 15:25:46 -0400

    Basic site changes based on url. If site not found, a 404 page is sent

--- index.js
+++ index.js
@@ -6,7 +6,44 @@ const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/hmtl");
-  res.end();
+
+  switch (req.url) {
+    case "/": {
+      fs.readFile("index.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(res.statusCode, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+      break;
+    }
+    case "/about": {
+      fs.readFile("about.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(res.statusCode, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+      break;
+    }
+    case "/contact-me": {
+      fs.readFile("contact-me.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(res.statusCode, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+      break;
+    }
+    default: {
+      fs.readFile("404.html", (err, data) => {
+        if (err) throw err;
+        res.writeHead(res.statusCode, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end;
+      });
+    }
+  }
 });
 
 server.listen(port, () => {
